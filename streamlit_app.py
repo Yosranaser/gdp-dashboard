0400292f40ed3aebe9624cb0f2510f368679444f
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-08-27 21:18:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
-
+import pickle
 st.title("model testing")
 engine_size=st.number_input('engine_size',min_value=0,max_value=10,value=1)
 clylinder=st.number_input('clylinder',min_value=0,max_value=10,value=1)

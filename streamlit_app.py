2734bf27f1967a21319b5ec1d8f43ca04d5b03c5
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-08-27 20:53:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import math
 from pathlib import Path
 
 st.title("model testing")
-engine_size=st.number_input('engine_size',min_value=0,max|_value=10,value=1)
-clylinder=st.number_input('clylinder',min_value=0,max|_value=10,value=1)
-fuelcompution=st.number_input('fuelcompution',min_value=0,max|_value=10,value=1)
+engine_size=st.number_input('engine_size',min_value=0,max_value=10,value=1)
+clylinder=st.number_input('clylinder',min_value=0,max_value=10,value=1)
+fuelcompution=st.number_input('fuelcompution',min_value=0,max_value=10,value=1)
 
                             
 
